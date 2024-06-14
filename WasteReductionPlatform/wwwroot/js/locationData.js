688d7f566aacd1ce896c35ba622c133249675302
Neenu Varghese <neenuvarghese1988@conestogac.on.ca>
2024-06-14 04:41:31 -0400

    new change

---
+++ WasteReductionPlatform/wwwroot/js/locationData.js
@@ -0,0 +1,13 @@
+﻿const locationData = {
+    "AB": {
+        "name": "Alberta",
+        "cities": ["Calgary", "Edmonton", "Red Deer"],
+        "postalCodes": ["T1A", "T2A", "T3A"]
+    },
+    "BC": {
+        "name": "British Columbia",
+        "cities": ["Vancouver", "Victoria", "Richmond"],
+        "postalCodes": ["V1A", "V2A", "V3A"]
+    },
+    // Add other provinces...
+};
---
+++ WasteReductionPlatform/wwwroot/js/register.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    $("#Province").change(function () {
+        var provinceCode = $(this).val();
+        updateAutocompleteOptions(provinceCode);
+    });
+
+    $("#City").on("input", function () {
+        validateCityPostalCode();
+    });
+
+    $("#PostalCode").on("input", function () {
+        validateCityPostalCode();
+    });
+
+    function updateAutocompleteOptions(provinceCode) {
+        if (provinceCode && locationData[provinceCode]) {
+            var cities = locationData[provinceCode].cities.map(city => `<option value="${city}">`).join('');
+            var postalCodes = locationData[provinceCode].postalCodes.map(code => `<option value="${code}">`).join('');
+            $("#CityList").html(cities);
+            $("#PostalCodeList").html(postalCodes);
+        } else {
+            $("#CityList").html('');
+            $("#PostalCodeList").html('');
+        }
+    }
+
+    function validateCityPostalCode() {
+        var provinceCode = $("#Province").val();
+        var city = $("#City").val();
+        var postalCode = $("#PostalCode").val();
+
+        var validCity = locationData[provinceCode]?.cities.includes(city);
+        var validPostalCode = locationData[provinceCode]?.postalCodes.includes(postalCode);
+
+        if (!validCity) {
+            $("#City").addClass("is-invalid");
+        } else {
+            $("#City").removeClass("is-invalid");
+        }
+
+        if (!validPostalCode) {
+            $("#PostalCode").addClass("is-invalid");
+        } else {
+            $("#PostalCode").removeClass("is-invalid");
+        }
+    }
+});
